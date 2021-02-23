59550f71c1770d88edcd6c8913e096fb602210c9
Krzysztof Giblewski <krzysztofgiblewski@gmail.com>
2021-02-23 19:02:44 +0100

    zarys już działa

---
+++ backup/unit1cmyk.pas
@@ -0,0 +1,96 @@
+unit unit1Cmyk;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ColorBox;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    ButtonC: TButton;
+    ButtonM: TButton;
+    ButtonY: TButton;
+    ButtonK: TButton;
+    ButtonRo: TButton;
+    EditC: TEdit;
+    EditM: TEdit;
+    EditY: TEdit;
+    EditK: TEdit;
+    EditRo: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    procedure ButtonCClick(Sender: TObject);
+    procedure ButtonKClick(Sender: TObject);
+    procedure ButtonMClick(Sender: TObject);
+    procedure ButtonRoClick(Sender: TObject);
+    procedure ButtonYClick(Sender: TObject);
+
+
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+  numerekC,numerekM,numerekY,numerekK,numerekRo,stringZListy:string;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+
+
+procedure TForm1.ButtonCClick(Sender: TObject);
+begin
+EditC.SelectAll;
+EditC.CopyToClipboard;
+
+end;
+
+
+procedure TForm1.ButtonKClick(Sender: TObject);
+begin
+  EditK.SelectAll;
+EditK.CopyToClipboard;
+end;
+
+procedure TForm1.ButtonMClick(Sender: TObject);
+begin
+  EditM.SelectAll;
+EditM.CopyToClipboard;
+end;
+
+procedure TForm1.ButtonYClick(Sender: TObject);
+begin
+  EditY.SelectAll;
+EditY.CopyToClipboard;
+end;
+
+
+
+procedure TForm1.ButtonRoClick(Sender: TObject);
+begin
+  EditRo.SelectAll;
+EditRo.CopyToClipboard;
+end;
+
+
+
+
+
+
+
+end.
+
